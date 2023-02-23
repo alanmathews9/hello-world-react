73dc116106d411d294d599d99999537cba55471a
alan <alangeorgemathews09@gmail.com>
2023-02-24 02:44:52 +0530

    Added Counter.js to understand use of setState

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message';
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
+      {/*<Message />*/}
       {/*<Greet name='alan' god='god'>
-        <p>im a shooter</p>  
+        <p>im a shooter</p>
       </Greet>
       <Greet name='lisa' god='lichu' />
       <button>curly</button>
