2759ea7a1caf7c04d0e8deca660e75299ad33657
alan <alangeorgemathews09@gmail.com>
2023-02-24 02:51:40 +0530

    Added Comments on counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,8 +10,9 @@ class Counter extends Component {
     }
 
     increment() { 
+        //state.count = this.state.count + 1
         this.setState({
-            count: this.state.count+1
+            count: this.state.count + 1
         })
         console.log(this.state.count)
     }
@@ -26,3 +27,10 @@ class Counter extends Component {
 }
 
 export default Counter
+
+/*if we use the state method(line commented with state,count)
+then the console value will keep on incrementing, however the ui will no rerender for each click,
+in that case on every click of the ui the value isnt displayed as required value it just remains at count 0
+
+that is where we use setState, here for every click the ui is re rendered so the changes are shown in the ui
+(this code under setState)*/
