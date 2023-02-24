890b8ed870eaf7c210f01a41f1420d91b48fa165
alan <alangeorgemathews09@gmail.com>
2023-02-25 01:45:48 +0530

    Added EventBind.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
-
+import EventBind from './components/EventBind';
 function App() {
   return (
     <div className="App">
+      <EventBind />
       {/*<ClassClick />*/}
       {/*<FunctionClick/>*/}
       {/*<Counter />*/}
