320e369770de3ad420398fd6f27bda2bd63d7d98
alan <alangeorgemathews09@gmail.com>
2023-02-25 02:05:43 +0530

    Added comments

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -32,4 +32,13 @@ class EventBind extends Component {
 
 export default EventBind
 
-/**/
+//TUTORIAL 14
+
+/*
+reason for binding is because of the way this keyword works in react
+event binding, the best way is to do binding in the constructor, this i referred to in line 12
+
+other 2 ways to do event binding is binding in the render method, 2nd method uses arrow function
+the commented lines 25 and 26 are methods 1 and 2
+however these methods are not ideal so we always opt for binding in constructor
+*/
