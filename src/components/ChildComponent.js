90066fc90a35f3ba3e3881cecaa4a254605899d0
alan <alangeorgemathews09@gmail.com>
2023-02-25 02:28:20 +0530

    Added comments

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -9,3 +9,11 @@ function ChildComponent(props) {
 }
 
 export default ChildComponent
+
+//TUTORIAL 15
+
+/*
+Here we use a function with the parameter as props
+and the return value is a button, with an on click event handler
+onclick is refered to props.greethandler meaning using props property we refer a method called greet handler
+*/
--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -24,3 +24,19 @@ import ChildComponent from './ChildComponent'
 }
 
 export default ParentComponent
+
+//TUTORIAL 15
+
+/*
+This is a class, with a constructor and a method in it called greet parent
+the aim to pass a method from child to parent and that is done using props
+in other words props are used for communication between parent and child
+in the render and return we call the Childcomponent(line 19) and a method called greetHandler 
+which is specified onclick of the button, refer childcomponent.js, and this greethandler refers to greetparent 
+which alerts browser with message
+note that childcomponent.js needs to imported 
+and also the since greetparent is used for a function outside, we need to bind as shown inside constructor
+
+
+now go to child component . js for the remaingin comments
+*/
