66d62c0f9f5b5d6994b75606241a3f0b9bb70760
alan <alangeorgemathews09@gmail.com>
2023-02-26 03:50:21 +0530

    Added Persons.js to understand refactoring

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import Person from './Person'
 function NameList() {
+    const persons = [
+        {
+            id: 1,
+            name: 'alan',
+            age: '21',
+            skill:'basketball'
+        },
+        {
+            id: 2,
+            name: 'ananya',
+            age: '20',
+            skill: 'sleeping'
+        },
+        {
+            id: 3,
+            name: 'lisa',
+            age: '20',
+            skill: 'laughing'
+        },
+
+    ]
+    const personList = persons.map(person => <Person person={person} />)
   return (
-    <div>NameList</div>
+    <div>
+          {personList}
+    </div>
   )
 }
 
