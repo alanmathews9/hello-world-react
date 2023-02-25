59acbe3efe8503f8ad9327a9a47e42f57df21e50
alan <alangeorgemathews09@gmail.com>
2023-02-26 03:25:14 +0530

    Updated UserGreetings.js

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react'
 
 class UserGreeting extends Component {
-  render() {
-    return (
-      <div>UserGreeting</div>
-    )
-  }
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         isLoggedin : true
+      }
+    }
+    render() {
+        //if-else method
+        // if (this.state.isLoggedin) {
+        //     return <div>Welcome alan</div>
+        // }
+        // else { 
+        //     return <div>Welcome guest</div>
+        // }
+
+        //element variables method
+        // let message
+        // if (this.state.isLoggedin) {
+        //     message = <div>Welcome alan</div>
+        // }
+        // else {
+        //     message = <div>Welcome guest</div>
+        // }
+        // return <div>{message}</div>
+        
+        //ternary conditional method
+        // return (
+        //     this.state.isLoggedin ? 
+        //     <div>Welcome alan</div> :
+        //     <div>Welcome guest</div>
+        // )
+        
+        //method 4(this is for when we want return for true and nothing for false)
+        return this.state.isLoggedin && <div>Welcome alan</div>
+    }
+        
 }
 
 export default UserGreeting
+
+//TUTORIAL 16
+
